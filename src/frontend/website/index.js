f4fcfeaf1a3ba2dcb4d9da57a535b598cdda7864
unknown <jun.park@Jun-P.CompacSort.local>
2015-04-11 17:01:45 +1200

    with date

--- src/frontend/website/index.js
+++ src/frontend/website/index.js
@@ -1,6 +1,32 @@
 function count_rabbits() {
     for(var i=1; i<=3; i++) {
         // operator + concatenates strings
-        alert("Rabbit "+i+" out of the hat!")
+        alert("Rabbit "+today+" out of the hat!")
     }
 }
+
+function displayLog() {
+	var objToday = new Date(),
+                weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+                dayOfWeek = weekday[objToday.getDay()],
+                domEnder = new Array( 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th' ),
+                dayOfMonth = today + (objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder[objToday.getDate()] : objToday.getDate() + domEnder[parseFloat(("" + objToday.getDate()).substr(("" + objToday.getDate()).length - 1))],
+                months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+                curMonth = months[objToday.getMonth()],
+                curYear = objToday.getFullYear(),
+                curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+                curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+                curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+                curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+	var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+	alert(today);
+	//return today;
+}
+
+function myFunction(a, b) {
+    return a * b;
+}
+
+document.getElementById("demo").innerHTML = displayLog();
+
+document.getElementById("demo2").innerHTML = myFunction(4, 3);
