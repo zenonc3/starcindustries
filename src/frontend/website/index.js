599f9c11a77cd7700dc5e84e11d5924dc1b56452
unknown <jun.park@Jun-P.CompacSort.local>
2015-04-11 19:08:23 +1200

    live time date working

--- src/frontend/website/index.js
+++ src/frontend/website/index.js
@@ -1,11 +1,11 @@
-function display_c() {
+function displayc() {
 var refresh=1000; // Refresh rate in milli seconds
-mytime=setTimeout('displayct()',refresh)
+mytime=setTimeout('displayct()',refresh);
 }
 
 function displayct() {
-var strcount
-
+var strcount;
+var x = new Date();
 document.getElementById('ct').innerHTML = x;
-tt=display_c();
+tt=displayc();
 }
