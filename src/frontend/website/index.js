c5a0e5a8234184ddcea45020b5fe413ab3a448cb
unknown <jun.park@Jun-P.CompacSort.local>
2015-04-11 23:07:00 +1200

    media and log stuff

--- src/frontend/website/index.js
+++ src/frontend/website/index.js
@@ -1,11 +1,26 @@
 function displayc() {
-var refresh=1000; // Refresh rate in milli seconds
-mytime=setTimeout('displayct()',refresh);
+	var refresh=1000; // Refresh rate in milli seconds
+	mytime=setTimeout('displayct()',refresh);
 }
 
 function displayct() {
-var strcount;
-var x = new Date();
-document.getElementById('ct').innerHTML = x;
-tt=displayc();
+	var strcount;
+	var x = new Date();
+	document.getElementById('ct').innerHTML = x;
+	tt=displayc();
+}
+
+function displayLog() {
+	var logMessageExists;
+	if (logMessageExists === true) {
+		var x = new Date();
+		var logMessageType = "Log";
+		var logMessage = "";
+	}	
+}
+
+function displayt() {
+	var y = new Date();
+	document.getElementById('t').innerHTML = y;
+	yy = displayt();
 }
