058f90541a19b18437ca9ca41934967444750bd1
Zenon Colaco <zncolaco@gmail.com>
2015-04-12 14:27:27 +1200

    Merged motion processor and web socket server with callbacks

--- src/processing/MotionProcessor.py
+++ src/processing/MotionProcessor.py
@@ -3,93 +3,123 @@ import time
 
 #class DataStream(threading.Thread):
 #	def run(self):
-host = '192.168.1.4'
-port = 5555
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.bind((host, port))
-counter=0
-counting=False
-action="starting"
-while 1:
-    try:
-	#print counter, counting
-	
-	if counter==20:
-		counting=False
-		counter=0
-	#Get full string
-	message, address = s.recvfrom(8192)
-	#print counter
-	#print message
-	if counting==True:	
-		counter=counter+1
-		
-	else:
-	
-		#strip space
-		message.strip(" ")
-		
-		#message.replace(" ,",",")
-		#delimit string
-		delimitedVariables=message.split(",");
-		
-		#Extract gyroscope information and convert to integer
-		if delimitedVariables[5]!=4:
-		
-			gx= float(delimitedVariables[6])
-			gy= float(delimitedVariables[7])
-			gz= float(delimitedVariables[8])
-			#print gx,gy, gz
-			threshold=5;
-			
-			maxValue = max(gx, gy, gz)
-			minValue = min(gx,gy,gz)
-			#print maxValue, minValue
-			
-			if(abs(maxValue)>abs(minValue)):
-				largest = maxValue
-				
-			else:
-				largest = minValue 
-			#Get action
-			if largest < -threshold and largest==gx:
-				action = "left"
-				counting=True
-				print action
-			elif largest > threshold-1 and largest==gx:
-				action = "right"
-				counting=True
-				print action
-			elif largest > threshold-2 and largest==gz: 
-				action= "up"
-				counting=True
-				print action
-			elif largest < -threshold and largest==gz:
-				action="down"
-				counting=True
-				print action
-			elif largest < -threshold and largest==gy:
-				action="twistleft"
-				counting=True
-				print action
-			elif largest > threshold and largest==gy:
-				action="twistright"
-				counting=True
-				print action
-				
-			else:
-				action="nomovement"
-				counting=False
 
-		#time.sleep(0.1)
-		#print gx,gy,gz
-			#print message
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        traceback.print_exc()
+class MotionProc:
+	
+	def init(self):
+		self.action = 'nomovement'
+		self.callback = ''
+    
+	def get_action(self):
+		if self.action == 'nomovement':
+			return False
+		else:
+			return self.action
+		
+	def set_callback(self, callbackref):
+		self.callback = callbackref
+	
+	def gesture_loop(self):
+		host = '192.168.1.4'
+		port = 5555
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+		s.bind((host, port))
+		counter=0
+		counting=False
+		action="starting"
+		while 1:
+			try:
+			#print counter, counting
+
+				if counter==20:
+					counting=False
+					counter=0
+				#Get full string
+				message, address = s.recvfrom(8192)
+				#print counter
+				#print message
+				if counting==True:	
+					counter=counter+1
+
+				else:
+
+					#strip space
+					message.strip(" ")
+
+					#message.replace(" ,",",")
+					#delimit string
+					delimitedVariables=message.split(",");
+
+					#Extract gyroscope information and convert to integer
+					if delimitedVariables[5]!=4:
+
+						gx= float(delimitedVariables[6])
+						gy= float(delimitedVariables[7])
+						gz= float(delimitedVariables[8])
+						#print gx,gy, gz
+						threshold=5;
+
+						maxValue = max(gx, gy, gz)
+						minValue = min(gx,gy,gz)
+						#print maxValue, minValue
+
+						if(abs(maxValue)>abs(minValue)):
+							largest = maxValue
+
+						else:
+							largest = minValue 
+						#Get action
+						if largest < -threshold and largest==gx:
+							self.action = "left"
+							counting=True
+							print self.action
+							self.callback(self.action)
+						elif largest > threshold-1 and largest==gx:
+							self.action = "right"
+							counting=True
+							print self.action
+							self.callback(self.action)
+						elif largest > threshold-2 and largest==gz: 
+							self.action= "up"
+							counting=True
+							print self.action
+							self.callback(self.action)
+						elif largest < -threshold and largest==gz:
+							self.action="down"
+							counting=True
+							print self.action
+							self.callback(self.action)
+						elif largest < -threshold and largest==gy:
+							self.action="twistleft"
+							counting=True
+							print self.action
+							self.callback(self.action)
+						elif largest > threshold and largest==gy:
+							self.action="twistright"
+							counting=True
+							print self.action
+							self.callback(self.action)
+
+						else:
+							action="nomovement"
+							counting=False
+
+				#time.sleep(0.1)
+				#print gx,gy,gz
+					#print message
+			except (KeyboardInterrupt, SystemExit):
+				raise
+			except:
+				traceback.print_exc()
+				
+				
+
+			
+if __name__ == "__main__":
+	mp = MotionProc()
+	mp.gesture_loop()
 	
 	
 
--- src/processing/ProcessServer.py
+++ src/processing/ProcessServer.py
@@ -1,11 +1,13 @@
 
 #NOTE: You will have to update the ip address in index.html
-
+import threading
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
 import tornado.template
 import datetime
+from MotionProcessor import MotionProc
+
 
 period_loop = ''
 
@@ -18,9 +20,11 @@ class WSHandler(tornado.websocket.WebSocketHandler):
   def open(self):
     print 'new connection'
     self.write_message("Hi, client: connection is made ...")
-    global period_loop 
-    period_loop = tornado.ioloop.PeriodicCallback(self.test, 500)
-    period_loop.start()
+    global period_loop
+    global mproc
+    mproc.set_callback(self.test)
+#    period_loop = tornado.ioloop.PeriodicCallback(self.test, 500)
+#    period_loop.start()
 #    tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=5), self.test)
 
   def on_message(self, message):
@@ -31,10 +35,13 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
   def on_close(self):
     print 'connection closed'
-    period_loop.stop()
+#    period_loop.stop()
     
-  def test(self):
-    self.write_message("scheduled!")
+  def test(self, action):
+    
+#    global mproc
+#    print mproc.get_action()
+    self.write_message(str(action))
              
   def check_origin(self, origin):
     return True
@@ -50,8 +57,17 @@ application = tornado.web.Application([
 
 
 if __name__ == "__main__":
+  
+  mproc = MotionProc()
+  t = threading.Thread(target=mproc.gesture_loop)
+  print 'thread created'
+  t.daemon = True
+  t.start()
+  print 'thread started'
+  
   application.listen(9090)
   main_loop = tornado.ioloop.IOLoop.instance()
   main_loop.start()
+
 #  while True:
 #    pass
