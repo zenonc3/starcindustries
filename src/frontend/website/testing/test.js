6c0cb0c381b92ad1cbe23c26836933da63f5691a
Zenon Colaco <zncolaco@gmail.com>
2015-04-12 17:35:16 +1200

    gesture integration with carousel

--- src/frontend/website/testing/test.js
+++ src/frontend/website/testing/test.js
@@ -1,10 +1,62 @@
 
-function fadein() {
-	$("#outer").hide();
-	setTimeout(show(),1000);
-	//$("#outer").fadeIn(3000);
+
+function setup(){
+  
+  var host = "ws://192.168.1.4:9090/ws";
+  var socket = new WebSocket(host);
+
+  // event handlers for websocket
+  if(socket){
+
+    socket.onopen = function(){
+      console.log("connection opened....");
+    }
+
+    socket.onmessage = function(msg){
+      console.log(msg.data);
+      sortActions(msg.data)
+      
+    }
+
+    socket.onclose = function(){
+      //alert("connection closed....");
+      console.log("The connection has been closed.");
+    }
+
+  }else{
+    console.log("invalid socket");
+  }
+
+  function sortActions(action){
+    if (action === "right" || action == "twistright"){
+      prevSlide();
+    }else if (action === "left" || action == "twistleft"){
+      nextSlide()
+    }
+  }
+  function fadein() {
+      $("#outer").hide();
+      setTimeout(show(),1000);
+      //$("#outer").fadeIn(3000);
+  }
+
+  function show() {
+      $("#outer").show();
+  }
+
+  function nextSlide() {
+    $('#carousel-example-generic').carousel('next');
+  }
+
+  function prevSlide() {
+    $('#carousel-example-generic').carousel('prev');
+  }
+
+  document.getElementById("testButton").addEventListener("click", function(){
+    console.log("pressed!");
+    prevSlide()
+  });
+  
 }
 
-function show() {
-    $("#outer").show();
-}
+window.onload = setup;
