c0eef6acf4563534867c5eb056a66a0ae0152a08
unknown <jun.park@Jun-P.CompacSort.local>
2015-04-12 20:32:28 +1200

    updated log/media page (single page)

--- src/frontend/website/testing/test.js
+++ src/frontend/website/testing/test.js
@@ -8,3 +8,31 @@ function fadein() {
 function show() {
     $("#outer").show();
 }
+
+function displayc() {
+	var refresh=1000; // Refresh rate in milli seconds
+	mytime=setTimeout('displayct()',refresh);
+}
+
+function displayct() {
+	var strcount;
+	var x = new Date();
+	//document.getElementById('ct').innerHTML = x;
+	tt=displayc();
+}
+
+function displayLog() {
+	var logMessageExists;
+	if (logMessageExists === true) {
+		var x = new Date();
+		var logMessageType = "Log";
+		var logMessage = "";
+	}	
+}
+
+function displayt() {
+	var y = new Date();
+	//var z = y.getTime()
+	document.getElementById("t").innerHTML = y;
+	yy = displayt();
+}
